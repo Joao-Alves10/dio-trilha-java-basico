8238d1e0806709a66e4fb95e1523ad998688bdec
João Alves <125817183+Joao-Alves10@users.noreply.github.com>
2023-03-01 17:03:56 -0300

    Delete module-info.java

--- tipos-variaveis/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tiposvariaveis {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tiposvariaveis to javafx.fxml;
-    exports com.example.tiposvariaveis;
-}
